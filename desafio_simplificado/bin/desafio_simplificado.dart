aa11da5639f9c74941eff28f5e8d70ddb3608aaf
Mateus <rossijf2@gmail.com>
2022-09-15 13:51:18 -0300

    Initial Commit

---
+++ desafio_simplificado/bin/desafio_simplificado.dart
@@ -0,0 +1,5 @@
+import 'package:desafio_simplificado/desafio_simplificado.dart' as desafio_simplificado;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desafio_simplificado.calculate()}!');
+}
---
+++ desafio_simplificado/test/desafio_simplificado_test.dart
@@ -0,0 +1,8 @@
+import 'package:desafio_simplificado/desafio_simplificado.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
